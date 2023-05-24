977af10f9e82bcaa08ff8ecf25261cdcac076ef6
manucho254 <manuchoadero@gmail.com>
2023-05-24 12:42:53 +0000

    fixes to run exec

--- run_exec.c
+++ run_exec.c
@@ -19,10 +19,13 @@ void execute_commands(char *command, char **env, pid_t parent_pid)
 {
 	char **command_arr = NULL;
 
-	if (strcmp(command, "exit") == 0)
-	{
-		exit_application(getpid(), parent_pid);
-	}
+	/**
+	 * if (strcmp(command, "exit") == 0)
+	 * {
+	 * exit_application(getpid(), parent_pid)
+	 * }
+	 */
+
 	/** work in child processes */
 	command_arr = separate_string(command);
 
